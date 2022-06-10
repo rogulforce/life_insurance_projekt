eb086236f98ca951c35fdd725aadbb1f35f227aa
Ryszard <ryszardrolik@gmail.com>
2022-06-11 01:11:44 +0200

    Working Lee Carter code
    ARIMA is a waste of time, it does not work correctly. As was said in the article of Girosi and King 2007.

---
+++ cache/joblib/__main__-C%3A-Users-andrz-AppData-Local-Temp-ipykernel-990643322/rolling_window/func_code.py
@@ -0,0 +1,8 @@
+# first line: 1
+def rolling_window(matrix, column: str = 'Female_l', age = 65, start_of_test = 53, end_of_test = 62, window_length = 41):
+    fm = matrix.pivot(index="Age", columns="Year", values=column)
+    rolling_window_forecasts = np.ones(end_of_test - start_of_test)*-999
+    for i in tqdm(range(start_of_test, end_of_test)):
+        fm_train = fm.iloc[:, i - window_length:i]
+        rolling_window_forecasts[i - start_of_test] = predict_rolling(fm_train, age = age)
+    return rolling_window_forecasts
